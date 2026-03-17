6084ab65fce27513378451340df54851a19c695c
knaaps <vinayak4x@gmail.com>
2026-03-17 14:39:34 +0530

    remove ablation cache, utility scripts, and update gitignore.

--- get_orig.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, './src')
-from legacylens.analysis.parser import extract_methods_from_file
-
-res = extract_methods_from_file('src/petclinic/owner/OwnerController.java', 'java')
-tgt = [n for n in res if n['name'] == 'OwnerController.processFindForm'][0]
-print("ORIGINAL CODE:")
-print(tgt['code'])
--- parse_trace.py
+++
@@ -1,4 +0,0 @@
-import json, sys
-data = json.load(open('results/regen_trace_processFindForm.json'))
-print(data['iterations'][0].keys())
-print(data['iterations'][0]['regeneration_response'][:200])
